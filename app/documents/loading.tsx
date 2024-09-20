1a729ecb869155a5550c9afe31fc53222b756f55
icarodredd <icarog854@gmail.com>
2024-09-20 16:01:41 -0300

    add loading animation

---
+++ app/documents/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  // Or a custom loading skeleton component
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <p className="text-7xl text-purple animate-spin">↻</p>
+    </div>
+  );
+}
