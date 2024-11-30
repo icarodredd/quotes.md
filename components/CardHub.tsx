b0abe94dc55c8787994ec7df8fbe948cd0a8cdc1
icarodredd <icarog854@gmail.com>
2024-11-30 15:20:09 -0300

    align doc icon on top

--- components/CardHub.tsx
+++ components/CardHub.tsx
@@ -1,19 +1,19 @@
-"use client";
+'use client';
 
-import { NoteType } from "./Documents";
-import { Button } from "./ui/button";
-import docicon from "../public/Vectordoc.png";
-import Image from "next/image";
+import { NoteType } from './Documents';
+import { Button } from './ui/button';
+import docicon from '../public/Vectordoc.png';
+import Image from 'next/image';
 import {
   Card,
   CardHeader,
   CardTitle,
   CardContent,
   CardFooter,
-} from "./ui/card";
-import { Dispatch, SetStateAction, useState } from "react";
-import JSZip from "jszip";
-import FileSaver from "file-saver";
+} from './ui/card';
+import { Dispatch, SetStateAction, useState } from 'react';
+import JSZip from 'jszip';
+import FileSaver from 'file-saver';
 
 function Note({
   note,
@@ -25,30 +25,32 @@ function Note({
   return (
     <button
       onClick={() => setSelected(note.id)}
-      className="rounded-lg p-4 hover:bg-purple/10"
+      className="flex items-start rounded-lg p-4 hover:bg-purple/10"
     >
-      <div className="flex justify-center">
-        <Image
-          src={docicon}
-          alt="doc icon"
-          width={60}
-          height={60}
-        />
+      <div>
+        <div className="flex justify-center">
+          <Image
+            src={docicon}
+            alt="doc icon"
+            width={60}
+            height={60}
+          />
+        </div>
+        <p className="text-purple text-center text-sm mt-2">{note.name}</p>
       </div>
-      <p className="text-purple text-center text-sm mt-2">{note.name}</p>
     </button>
   );
 }
 
 function CardHub({ result, token }: { result: NoteType[]; token: string }) {
-  const [selected, setSelected] = useState("");
+  const [selected, setSelected] = useState('');
 
   const handleClick = async (id: string) => {
-    if (id === "") return;
+    if (id === '') return;
     await fetch(
       `https://www.googleapis.com/drive/v3/files/${id}/export?mimeType=text/markdown`,
       {
-        method: "GET",
+        method: 'GET',
         headers: {
           Authorization: `Bearer ${token}`,
         },
@@ -59,16 +61,16 @@ function CardHub({ result, token }: { result: NoteType[]; token: string }) {
       //filtering header and footer of the documement
       .then((data) =>
         data.substring(
-          data.indexOf("*![][image2] ") + 13,
-          data.lastIndexOf("http://play.google.com/books/reader")
+          data.indexOf('*![][image2] ') + 13,
+          data.lastIndexOf('http://play.google.com/books/reader')
         )
       )
       //spliting the document into an array of notes
-      .then((data) => data.split("\n\n"))
+      .then((data) => data.split('\n\n'))
 
       //creating a new file for each note
       .then((data) =>
-        data.map((note) => new File([note], `Note`, { type: "text/markdown" }))
+        data.map((note) => new File([note], `Note`, { type: 'text/markdown' }))
       )
 
       //zipping the files in one and downloading
@@ -77,8 +79,8 @@ function CardHub({ result, token }: { result: NoteType[]; token: string }) {
         for (let i = 0; i < files.length; i++) {
           zip.file(`note_${i}.md`, files[i]);
         }
-        const zipBlob = await zip.generateAsync({ type: "blob" });
-        FileSaver.saveAs(zipBlob, "Notes.zip");
+        const zipBlob = await zip.generateAsync({ type: 'blob' });
+        FileSaver.saveAs(zipBlob, 'Notes.zip');
       })
 
       .catch((e) => console.log(e));
