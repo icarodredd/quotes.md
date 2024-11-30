609d8b2946ccc85c1a02f298bb909c30e77ccf52
icarodredd <icarog854@gmail.com>
2024-11-30 16:37:12 -0300

    add contact on footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,24 +1,36 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 function Footer() {
   return (
     <footer className="p-4 text-center ">
       <h1 className="text-lg">
-        Developed by{" "}
+        Developed by{' '}
         <Link
           className="hover:underline"
-          href={"https://www.linkedin.com/in/icarodredd/"}
+          href={'https://www.linkedin.com/in/icarodredd/'}
         >
           icarodredd
         </Link>
       </h1>
       <div className="space-x-2 text-xs">
-        <Link className="hover:underline" href={"/privacy-policy"}>
+        <Link
+          className="hover:underline"
+          href={'/privacy-policy'}
+        >
           Privacy Policy
         </Link>
-        <Link className="hover:underline" href={"/terms-of-service"}>
+        <Link
+          className="hover:underline"
+          href={'/terms-of-service'}
+        >
           Terms of Service
         </Link>
+        <a
+          className="hover:underline"
+          href="https://linkedin.com/in/icarodredd"
+        >
+          Contact
+        </a>
       </div>
     </footer>
   );
